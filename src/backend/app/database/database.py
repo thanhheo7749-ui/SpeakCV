b3e7a9f051333fe323f4998ad580ff3b809a9b19
Thanh Hoang <hoangthanh@gmail.com>
2026-03-02 19:56:01 +0700

    build(root): setup docker-compose for frontend and backend

--- src/backend/app/database/database.py
+++ src/backend/app/database/database.py
@@ -2,20 +2,18 @@
 # This project is licensed under the MIT License.
 # See the LICENSE file in the project root for more information.
 
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import urllib.parse
+from sqlalchemy.ext.declarative import declarative_base
 
-# --- CẤU HÌNH MYSQL ---
-
-PASSWORD = "thanhhoang@123" 
-encoded_password = urllib.parse.quote_plus(PASSWORD)
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:{encoded_password}@localhost:3306/speakcv"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "mysql+pymysql://root:thanhhoang%40123@localhost:3306/speakcv"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
